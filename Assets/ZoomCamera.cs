bf09e5683f7f3c2ca9c93babc19f0ccb5ac78ed3
matumaru0825 <bun_3x3_bun@icloud.com>
2026-05-09 12:05:58 +0900

    ５月９日

--- Assets/ZoomCamera.cs
+++ Assets/ZoomCamera.cs
@@ -8,21 +8,24 @@ public class ZoomCamera : MonoBehaviour
     [SerializeField] private float defaultFov = 60f;
 
     [SerializeField] private float LifeTimeSec = 2f;
+    private float LifeFrame = 0f;
+
 
     [SerializeField] private float FoVDegresShiftPerSec = -2.5f;
 
-    [SerializeField] private float LifeFrame = 0f;
+
+    [SerializeField] private bool fpause;  //途中で一時停止するためのフラグ
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(TaregetCamera == null)
+        if (TaregetCamera == null)
         {
             TaregetCamera = this.gameObject.GetComponent<Camera>();
-            if(TaregetCamera == null)
+            if (TaregetCamera = null)
             {
                 Debug.LogError("カメラが見つかりません");
-            }    
+            }
         }
         Initialize();
     }
@@ -30,23 +33,30 @@ public class ZoomCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //奥に動く
-        //this.TaregetCamera.transform.position += Vector3(0 , 0, 0.01f);
+        //ポーズフラグが立っているときは、何もしないでUpdateを打ち切る
+        if (fpause) return;
+
         LifeTimeSec -= Time.deltaTime;
-        if(LifeFrame > 0)
+
+        if (LifeFrame > 0)
         {
             //zoom
-            this.TaregetCamera.fieldOfView += FoVDegresShiftPerSec * Time.deltaTime ;
+            this.TaregetCamera.fieldOfView += FoVDegresShiftPerSec * Time.deltaTime;
         }
     }
 
     public void Initialize()
     {
-        if(TaregetCamera != null)
+        if (TaregetCamera != null)
         {
             TaregetCamera.fieldOfView = defaultFov;
         }
-        
+
         LifeFrame = LifeTimeSec;
+    }
+
+    public void Pause()
+    {
+        fpause = !fpause;
     }
 }
